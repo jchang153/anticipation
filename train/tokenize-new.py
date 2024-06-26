09be3c8f1f13e10dfb5129f17168f9e0f233a982
caenopy <nickpbecker@gmail.com>
2024-06-25 18:46:42 -0700

    add non-live augmentations, disable live by default

--- train/tokenize-new.py
+++ train/tokenize-new.py
@@ -44,9 +44,9 @@ def prepare_triplet_midi(midifile, vocab):
     return events
 
 def control_prefix(instruments, human_instruments, task, vocab):
+
     task = vocab['task'][task]
     instr_offset = vocab['instrument_offset']
-    human_instr_offset = vocab['human_instrument_offset']
     separator = vocab['separator']
     pad = vocab['pad']
 
@@ -55,10 +55,11 @@ def control_prefix(instruments, human_instruments, task, vocab):
     instr_controls = sorted(instruments)
     instr_controls = [instr_offset + instr for instr in instruments]
 
-    human_instr_controls = sorted(human_instruments)
-    human_instr_controls = [human_instr_offset + instr for instr in human_instruments]
-
-    instr_controls = instr_controls + human_instr_controls
+    if human_instruments is not None:
+        human_instr_offset = vocab['human_instrument_offset']
+        human_instr_controls = sorted(human_instruments)
+        human_instr_controls = [human_instr_offset + instr for instr in human_instruments]
+        instr_controls = instr_controls + human_instr_controls
 
     vocab_size = vocab['config']['size']
     assert max(instr_controls) < vocab_size
@@ -78,8 +79,48 @@ def control_prefix(instruments, human_instruments, task, vocab):
 
     return z_start, z_cont
 
+def extract_spans(all_events, rate, vocab):
+    events = []
+    controls = []
+    span = True
+    next_span = end_span = vocab['time_offset']+0
+    for time, dur, note in zip(all_events[0::3],all_events[1::3],all_events[2::3]):
+        assert(note not in [vocab['separator'], vocab['rest']]) # shouldn't be in the sequence yet
 
-def pack_tokens(sequences, output, idx, vocab, prepare, prefix, seqlen, piano_human_part=True):
+        # end of an anticipated span; decide when to do it again (next_span)
+        if span and time >= end_span:
+            span = False
+            next_span = time+int(TIME_RESOLUTION*np.random.exponential(1./rate))
+
+        # anticipate a 3-second span
+        if (not span) and time >= next_span:
+            span = True
+            end_span = time + DELTA*TIME_RESOLUTION
+
+        if span:
+            # mark this event as a control
+            controls.extend([vocab['control_offset']+time, vocab['control_offset']+dur, vocab['control_offset']+note])
+        else:
+            events.extend([time, dur, note])
+
+    return events, controls
+
+ANTICIPATION_RATES = 10
+def extract_random(all_events, rate, vocab):
+    events = []
+    controls = []
+    for time, dur, note in zip(all_events[0::3],all_events[1::3],all_events[2::3]):
+        assert(note not in [vocab['separator'], vocab['rest']]) # shouldn't be in the sequence yet
+
+        if np.random.random() < rate/float(ANTICIPATION_RATES):
+            # mark this event as a control
+            controls.extend([vocab['control_offset']+time, vocab['control_offset']+dur, vocab['control_offset']+note])
+        else:
+            events.extend([time, dur, note])
+
+    return events, controls
+
+def pack_tokens(sequences, output, idx, vocab, prepare, prefix, seqlen, live=False, piano_human_part=True):
     vocab_size = vocab['config']['size']
     pad = vocab['pad']
     files = bad_files = seqcount = 0
@@ -99,80 +140,128 @@ def pack_tokens(sequences, output, idx, vocab, prepare, prefix, seqlen, piano_hu
                 continue
 
             # record the original end time before extracting control tokens
-            end_time = ops.max_time(events, seconds=False)
-
-            # anticipation happens here (extract control tokens)
-            #   * extract the chord sequence to anticipate
-            #   * extract the (randomly selected) "human" sequence to anti-anticipate
-            
+            end_time = ops.max_time(events, seconds=False)          
             instruments = sorted(list(ops.get_instruments(events).keys()))
 
-            chords_program_num = vocab['chord_instrument'] - vocab['instrument_offset']
+            if live:
+                chords_program_num = vocab['chord_instrument'] - vocab['instrument_offset']
 
-            # extract the chord sequence to anticipate
-            # check if vocab['chord_instrument'] is in instruments
-            chord_controls = None
-            if chords_program_num in instruments:
-                # we want at least 2 instruments excluding chords
-                if len(instruments) < 3:
-                    continue
                 # extract the chord sequence to anticipate
-                events, chord_controls = extract_instruments(events, [chords_program_num], vocab)
-                instruments.remove(chords_program_num)
+                chord_controls = None
+                if chords_program_num in instruments:
+                    if len(instruments) < 3:
+                        continue
+                    events, chord_controls = extract_instruments(events, [chords_program_num], vocab)
+                    instruments.remove(chords_program_num)
+                else:
+                    if len(instruments) < 2:
+                        continue
+
+                # extract piano or randomly selected "human" sequence to anti-anticipate
+                if piano_human_part:
+                    human = [0]
+                else:
+                    human = np.random.choice(instruments, 1, replace=False)
+
+                instruments.remove(human[0])
+                events, human_controls = extract_instruments(events, human, vocab)
+
+                # get the global control tokens for this sequence
+                # do this before padding because some ops don't handle REST properly
+                z_start, z_cont = prefix(instruments, human)
+
+                # add rest tokens to events after extracting control tokens
+                # (see Section 3.2 of the paper for why we do this)
+                events = ops.pad(events, end_time)
+
+                # interleave control tokens
+                tokens, chord_controls, human_controls = ops.anticipate_and_anti_anticipate(events, chord_controls, human_controls, chord_delta=DELTA*TIME_RESOLUTION, human_delta=HUMAN_DELTA*TIME_RESOLUTION)
+
+                # write out full contexts to file
+                concatenated_tokens.extend(z_start + tokens)
+                while len(concatenated_tokens) >= seqlen-len(z):
+                    seq = concatenated_tokens[0:seqlen-len(z)]
+                    concatenated_tokens = concatenated_tokens[len(seq):]
+
+                    # relativize time to the context 
+                    seq = ops.translate(seq, -ops.min_time(seq, seconds=False), seconds=False)
+                    assert ops.min_time(seq, seconds=False) == 0
+
+                    # if notes in the chunk exceed vocab max time, skip it
+                    if ops.max_time(seq, seconds=False) >= vocab['config']['max_time']:
+                        continue
+
+                    seq = z + seq
+
+                    assert max(seq) < vocab_size
+                    outfile.write(' '.join([str(tok) for tok in seq]) + '\n')
+                    z = z_cont # update the global control prompt (if it changed)
+                    seqcount += 1
             else:
-                # we want at least 2 instruments
-                if len(instruments) < 2:
-                    continue
 
-            # extract piano or randomly selected "human" sequence to anti-anticipate
-            if piano_human_part:
-                human = [0]
-            else:
-                human = np.random.choice(instruments, 1, replace=False)
+                all_events = events.copy()
 
-            instruments.remove(human[0])
-            events, human_controls = extract_instruments(events, human, vocab)
+                # different random augmentations
+                augment_factor = 30
+                for k in range(augment_factor): # using default augment_factor from original paper
+                    if k % 10 == 0:
+                        # no augmentation
+                        events = all_events.copy()
+                        controls = []
+                    elif k % 10 == 1:
+                        # span augmentation
+                        lmbda = .05
+                        events, controls = extract_spans(all_events, lmbda, vocab)
+                    elif k % 10 < 6:
+                        # random augmentation
+                        r = np.random.randint(1,ANTICIPATION_RATES)
+                        events, controls = extract_random(all_events, r, vocab)
+                    else:
+                        if len(instruments) > 1:
+                            # instrument augmentation: at least one, but not all instruments
+                            u = 1+np.random.randint(len(instruments)-1)
+                            subset = np.random.choice(instruments, u, replace=False)
+                            events, controls = extract_instruments(all_events, subset, vocab)
+                        else:
+                            # no augmentation
+                            events = all_events.copy()
+                            controls = []
 
-            # get the global control tokens for this sequence
-            # do this before padding because some ops don't handle REST properly
-            z_start, z_cont = prefix(instruments, human)
+                    z_start, z_cont = prefix(instruments, None)
+                    events = ops.pad(events, end_time)
+    
+                    tokens, controls = ops.anticipate(events, controls, DELTA*TIME_RESOLUTION)
+                    assert len(controls) == 0 # should have consumed all controls (because of padding)
 
-            # add rest tokens to events after extracting control tokens
-            # (see Section 3.2 of the paper for why we do this)
-            events = ops.pad(events, end_time)
+                    # write out full contexts to file
+                    concatenated_tokens.extend(z_start + tokens)
+                    while len(concatenated_tokens) >= seqlen-len(z):
+                        seq = concatenated_tokens[0:seqlen-len(z)]
+                        concatenated_tokens = concatenated_tokens[len(seq):]
 
-            # interleave control tokens
-            tokens, chord_controls, human_controls = ops.anticipate_and_anti_anticipate(events, chord_controls, human_controls, chord_delta=DELTA*TIME_RESOLUTION, human_delta=HUMAN_DELTA*TIME_RESOLUTION)
+                        # relativize time to the context 
+                        seq = ops.translate(seq, -ops.min_time(seq, seconds=False), seconds=False)
+                        assert ops.min_time(seq, seconds=False) == 0
 
-            # write out full contexts to file
-            concatenated_tokens.extend(z_start + tokens)
-            while len(concatenated_tokens) >= seqlen-len(z):
-                seq = concatenated_tokens[0:seqlen-len(z)]
-                concatenated_tokens = concatenated_tokens[len(seq):]
+                        # if notes in the chunk exceed vocab max time, skip it
+                        if ops.max_time(seq, seconds=False) >= vocab['config']['max_time']:
+                            continue
 
-                # relativize time to the context 
-                seq = ops.translate(seq, -ops.min_time(seq, seconds=False), seconds=False)
-                assert ops.min_time(seq, seconds=False) == 0
+                        seq = z + seq
 
-                # if notes in the chunk exceed vocab max time, skip it
-                if ops.max_time(seq, seconds=False) >= vocab['config']['max_time']:
-                    continue
-
-                seq = z + seq
-
-                assert max(seq) < vocab_size
-                outfile.write(' '.join([str(tok) for tok in seq]) + '\n')
-                z = z_cont # update the global control prompt (if it changed)
-                seqcount += 1
+                        assert max(seq) < vocab_size
+                        outfile.write(' '.join([str(tok) for tok in seq]) + '\n')
+                        z = z_cont # update the global control prompt (if it changed)
+                        seqcount += 1
 
     return (files, bad_files, seqcount)
 
 
-def preprocess_midi(midifiles, output, seqlen, task, vocab, idx):
+def preprocess_midi(midifiles, output, seqlen, task, vocab, idx, live=False):
     prefix = lambda instruments, human_instruments: control_prefix(instruments, human_instruments, task, vocab)
     prepare = lambda mid: prepare_triplet_midi(mid, vocab)
 
-    return pack_tokens(midifiles, output, idx, vocab, prepare, prefix, seqlen=seqlen)
+    return pack_tokens(midifiles, output, idx, vocab, prepare, prefix, seqlen=seqlen, live=live)
 
 
 preproc_func = {
@@ -208,7 +297,7 @@ def main(args):
 
     print('Processing...')
     if args.debug:
-        results = preproc_func[args.task](shards[0], outputs[0], args.context, args.task, vocab[0], 0)
+        results = preproc_func[args.task](shards[0], outputs[0], args.context, args.task, vocab[0], 0, args.live)
         results = [results]
     else:
         with Pool(processes=args.workers, initargs=(RLock(),), initializer=tqdm.set_lock) as pool:
@@ -230,5 +319,6 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--vocab', default='triplet-midi', help='name of vocabulary to use for tokenization')
     parser.add_argument('--workers', type=int, default=16, help='number of workers/shards')
     parser.add_argument('--debug', action='store_true', help='debugging (single shard; non-parallel)')
+    parser.add_argument('--live', action='store_true', help='tokenize for live mode (chords and human part)')
 
     main(parser.parse_args())
